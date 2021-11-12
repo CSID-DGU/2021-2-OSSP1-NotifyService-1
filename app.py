3ca980a7216b59a31e78366acdd8d1ca85b3553e
Hoyasdf <alsghqlgodrl@gmail.com>
2021-11-13 07:28:32 +0900

    test 카카오skill

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')              # <-- 추가된 3줄
-def index():                 # <
-    return "Hello world!"    # <
+@app.route('/')              
+def index():                 
+    return "Hello world!"   
+
+@app.route('/department', methods=['POST'])
+def department():
+    req = request.get_json()
+
+    college = req["action"]["detailParams"]["college"]["value"]   #json파일 읽기
+    department = req["action"]["detailParams"]["department"]["value"]
+
+    answer = college + "대학\n" + department
+
+    #답변 설정
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs" : [
+                {
+                    "simpleText": {
+                        "text": answer
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(res)
