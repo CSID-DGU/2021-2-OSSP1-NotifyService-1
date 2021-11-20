9954eb6af4f7fd9998164767f7e54763404e7edf
wonny7447 <73789701+wonny7447@users.noreply.github.com>
2021-11-21 02:19:43 +0900

    add 크롤링 작업

--- crawling.py
+++ crawling.py
@@ -40,68 +40,15 @@ worksheet.write(0, 2, '제목')
 worksheet.write(0, 3, '링크')
 
 url_list = [
-    ['https://kor-cre.dongguk.edu/?page_id=282', '문과대학 국어국문문예창작학부']
-		, ['https://english.dongguk.edu/?page_id=250', '문과대학 영어영문학부 ']
-		, ['https://english.dongguk.edu/?page_id=259', '문과대학 영어영문학부 ']
-		, ['https://dj.dongguk.edu/?page_id=203', '문과대학 일본학과 ']
-		, ['https://dj.dongguk.edu/?page_id=225', '문과대학 일본학과 ']
-		, ['https://china.dongguk.edu/?page_id=208', '문과대학 중어중문학과 ']
-		, ['https://sophia.dongguk.edu/?page_id=230', '문과대학 철학과 ']
-		, ['https://history.dongguk.edu/?page_id=505', '문과대학 사학과 ']
-		, ['https://chem.dongguk.edu/?page_id=288', '이과대학 화학과 ']
-		, ['https://chem.dongguk.edu/?page_id=288', '이과대학 통계학과 ']
-		, ['https://stat.dongguk.edu/?page_id=437', '이과대학 통계학과 ']
-		, ['https://physics.dongguk.edu/?page_id=324', '이과대학 물리반도체과학부 ']
-		, ['https://physics.dongguk.edu/?page_id=326', '이과대학 물리반도체과학부 ']
-		, ['https://politics.dongguk.edu/?page_id=18518', '사회과학대학 정치외교학과 ']
-		, ['https://politics.dongguk.edu/?page_id=18522', '사회과학대학 정치외교학과 ']
-		, ['https://pa.dongguk.edu/?page_id=233', '사회과학대학 행정학과 ']
-		, ['https://nk.dongguk.edu/?page_id=225', '사회과학대학 북한학과 ']
-		, ['https://econ.dongguk.edu/?page_id=262', '사회과학대학 경제학과 ']
-		, ['https://econ.dongguk.edu/?page_id=297', '사회과학대학 경제학과']
-		, ['http://itrade.dongguk.edu/?page_id=150', '사회과학대학 국제통상학과 ']
-		, ['https://sociology.dongguk.edu/?page_id=211', '사회과학대학 사회학과 ']
-		, ['https://welfare.dongguk.edu/?page_id=209', '사회과학대학 사회복지학과 ']
-		, ['https://police.dongguk.edu/?page_id=18349', '경찰사법대학 경찰행정학부 ']
-		, ['https://mgt.dongguk.edu/?page_id=18326', '경영대학 경영학과 ']
-		, ['https://acc.dongguk.edu/?page_id=18334', '경영대학 회계학과 ']
-		, ['https://acc.dongguk.edu/?page_id=1087707', '경영대학 회계학과 ']
-		, ['https://acc.dongguk.edu/?page_id=18328', '경영대학 회계학과 ']
-		, [' http://mis.dongguk.edu/?page_id=269', '경영대학 경영정보학과 ']
-		, ['https://bio.dongguk.edu/?page_id=18531', '바이오시스템대학 바이오환경과학과 ']
-		, ['https://lifescience.dongguk.edu/?page_id=150', '바이오시스템대학 생명과학과 ']
-		, ['https://food.dongguk.edu/?page_id=243', '바이오시스템대학 식품생명공학과 ']
-		, [' http://mbt.dongguk.edu/?page_id=241', '바이오시스템대학 의생명공학과 ']
-		, ['https://civil.dongguk.edu/?page_id=269', '공과대학 건설환경공학과 ']
-		, ['https://civil.dongguk.edu/?page_id=271', '공과대학 건설환경공학과 ']
-		, ['https://civil.dongguk.edu/?page_id=273', '공과대학 건설환경공학과 ']
-		, [' https://civil.dongguk.edu/?page_id=277', '공과대학 건설환경공학과 ']
-		, ['https://archi.dongguk.edu/?page_id=18387', '공과대학 건축공학부 ']
-		, ['https://archi.dongguk.edu/?page_id=18394', '공과대학 건축공학부 ']
-		, ['https://mecha.dongguk.edu/?page_id=207', '공과대학 기계로봇에너지공학과 ']
-		, ['https://mecha.dongguk.edu/?page_id=332', '공과대학 기계로봇에너지공학과 ']
-		, ['https://me.dongguk.edu/?page_id=249', '공과대학 융합에너지신소재공학과 ']
-		, ['https://dee.dongguk.edu/?page_id=553', '공과대학 전자전기공학부 ']
-		, ['https://dee.dongguk.edu/?page_id=555', '공과대학 전자전기공학부 ']
-		, ['https://dee.dongguk.edu/?page_id=557', '공과대학 전자전기공학부 ']
-		, ['https://ice.dongguk.edu/?page_id=18518', '공과대학 정보통신공학전공 ']
-		, ['https://cse.dongguk.edu/?page_id=799', '공과대학 컴퓨터공학전공']
-		, ['https://cse.dongguk.edu/?page_id=827', '공과대학 컴퓨터공학전공']
-		, ['https://chembioeng.dongguk.edu/?page_id=207', '공과대학 화공생물공학과 ']
-		, ['https://chembioeng.dongguk.edu/?page_id=83742', '공과대학 화공생물공학과 ']
-		, ['https://education.dongguk.edu/?page_id=230', '사범대학 교육학과 ']
-		, ['https://duce.dongguk.edu/?page_id=245', '사범대학 국어교육과 ']
-		, ['https://geoedu.dongguk.edu/?page_id=148', '사범대학 지리교육과 ']
-		, ['https://dume.dongguk.edu/?page_id=223', '사범대학 수학교육과 ']
-		, ['https://homeedu.dongguk.edu/?page_id=211', '사범대학 가정교육과 ']
-		, ['https://pe.dongguk.edu/?page_id=230', '사범대학 체육교육과 ']
-		, ['https://theatre.dongguk.edu/?page_id=391', '예술대학 연극학부 ']
+    'https://www.dongguk.edu/mbs/kr/jsp/board/list.jsp?boardId=3646&id=kr_010802000000'  # 일반
+    , 'https://www.dongguk.edu/mbs/kr/jsp/board/list.jsp?boardId=3638&id=kr_010801000000'  # 학사
+    , 'https://www.dongguk.edu/mbs/kr/jsp/board/list.jsp?boardId=3654&id=kr_010803000000'  # 입시
+    , 'https://www.dongguk.edu/mbs/kr/jsp/board/list.jsp?boardId=3662&id=kr_010804000000'  # 장학
+    , 'https://www.dongguk.edu/mbs/kr/jsp/board/list.jsp?boardId=9457435&id=kr_010807000000'  # 국제
+    , 'https://www.dongguk.edu/mbs/kr/jsp/board/list.jsp?boardId=11533472&id=kr_010808000000'  # 학술/행사
 ]
 
-for list in url_list:
-		
-		# url_list가 2차원 배열이므로, 공지사항 링크를 변수 url에 저장
-    url = list[0]
+for url in url_list:
 
     # url_list의 loop를 돌면서 url이 변경될 때 마다 현재 페이지를 1로 설정
     curPage = 1
@@ -113,7 +60,7 @@ for list in url_list:
         print('original url : ' + url)
 
         # 변경된 url에 페이지 번호를 붙임
-        url_change = url + f'&pageid={curPage}'
+        url_change = url + f'&spage={curPage}'
         print('changed url : ' + url_change)
         print('-------------------------------------------------')
 
@@ -125,10 +72,8 @@ for list in url_list:
         soup = BeautifulSoup(html, 'html.parser')
 
         # 게시글 리스트 선택
-        board_list = soup.select('#kboard-default-list > div.kboard-list > table > tbody > tr')
-        
-				# 카테고리 정보는 크롤링하지 않고 2차원 배열에 저장한 값을 읽음.
-				category = list[1]
+        board_list = soup.select('#board_list > tbody > tr')
+        category = soup.select_one('p.location > strong').text.strip()
 
         for board in board_list:
 
@@ -136,14 +81,14 @@ for list in url_list:
             # 고정된 공지는 td > img 형태인데, 이를 text로 변환하면 공백이 됨
             notice = board.select_one('td').text.strip()
 
-            if notice == "공지사항":  # 공백인 경우 고정공지이므로 크롤링 하지 않음
+            if notice == "":  # 공백인 경우 고정공지이므로 크롤링 하지 않음
                 continue
             else:  # 값이 있는 경우 일반공지로, 크롤링 진행
                 # 게시글 제목, 링크
-                name = board.select_one('td.kboard-list-title > div > a').text.strip()
-                link = url + board.select_one('td.kboard-list-title > div > a').get('href')
+                name = board.select_one('td.title > a').text.strip()
+                link = 'https://www.dongguk.edu/mbs/kr/jsp/board/' + board.select_one('td.title > a').get('href')
 
-                print('[' + notice + '][' + category + ']' + name + ' >> ' + link)
+                print('[' + notice + ']' + name + ' >> ' + link)
 
                 # 엑셀 저장(텍스트)
                 worksheet.write(excel_row, 0, category)  # 분류
