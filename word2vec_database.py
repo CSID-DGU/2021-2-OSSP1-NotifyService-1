8eba3c4845257be2328c739d08fb58e09aeef128
unknown <lsj8249@naver.com>
2021-11-25 20:27:12 +0900

    refactor | 유사단어 학습과 링크 리턴하는 코드 분리

--- word2vec_database.py
+++ word2vec_database.py
@@ -17,38 +17,46 @@ engine = create_engine('postgresql://jrbysnbvqyvmie:4a2d878446a2864c6c7b9b16b965
 Session = sessionmaker()
 Session.configure(bind=engine)
 
-session = Session()
-train_data=session.query(Crawl.title).all()
-train_data=pd.DataFrame(train_data,columns = ['제목'])
-train_data['제목']= train_data['제목'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")#데이터 정규표현식 -> 특수문자 제거
+def findSynonym():
+    session = Session()
+    train_data=session.query(Crawl.title).all()
+    train_data=pd.DataFrame(train_data,columns = ['제목'])
+    train_data['제목']= train_data['제목'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")#데이터 정규표현식 -> 특수문자 제거
 
     # 불용어 처리
-stop_words= []
-with open('stopword.txt', encoding='utf-8') as f:
-    for i in f:
-        stop_words.append(i.strip())
+    stop_words= []
+    with open('stopword.txt', encoding='utf-8') as f:
+        for i in f:
+            stop_words.append(i.strip())
 
-okt = Okt()
+    okt = Okt()
 
-tokenized_data = []
-for sentence in tqdm.tqdm(train_data['제목']):
-    tokenized_sentence = okt.nouns(sentence) # 토큰화 -> noun으로 하는 건 어떰? --> 형태소 말고 명사로 단위로 쪼갬
-    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stop_words] # 불용어 제거
-    tokenized_data.append(stopwords_removed_sentence)
+    tokenized_data = []
+    for sentence in tqdm.tqdm(train_data['제목']):
+        tokenized_sentence = okt.nouns(sentence) # 토큰화 -> noun으로 하는 건 어떰? --> 형태소 말고 명사로 단위로 쪼갬
+        stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stop_words] # 불용어 제거
+        tokenized_data.append(stopwords_removed_sentence)
 
-model = Word2Vec(sentences = tokenized_data)# 학습 -> 모든것이 default로 되어있으므로 값 정해야 함 !
-    # print(model.wv.vectors.shape)
+    global model
+    model = Word2Vec(sentences = tokenized_data)# 학습 -> 모든것이 default로 되어있으므로 값 정해야 함 !
+        # print(model.wv.vectors.shape)
+    # 모델 저장
+    model.save('dataset.model')
 
-word_vectors=model.wv
-vocabs=word_vectors.vocab.keys()# 사전
-word_vectors_list=[word_vectors[v] for v in vocabs]
-
-# print(vocabs)
+    word_vectors=model.wv
+    vocabs=word_vectors.vocab.keys()# 사전
+    word_vectors_list=[word_vectors[v] for v in vocabs]
+    session.close()
+    # return model
     
-similar = model.wv.most_similar("연구")#사업과 가장 유사한 단어 
-for i in similar:
-    train_data=session.query(Crawl.link).filter(Crawl.title.like('%'+i[0]+'%')).all()
-    print(train_data)
+def findLink(set, keyword): # 학습결과를 model로 저장함
+    session = Session()
+    model = Word2Vec.load(set)
+    similar = model.wv.most_similar(keyword)#사업과 가장 유사한 단어 
+    for i in similar:
+        train_data=session.query(Crawl.link).filter(Crawl.title.like('%'+i[0]+'%')).all()
+        print(train_data)
+    session.close()
 
-
-session.close()
+# findSynonym()
+findLink('dataset.model', '장학')
