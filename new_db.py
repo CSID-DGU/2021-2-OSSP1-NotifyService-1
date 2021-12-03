48d04d92db60915586814cd1f8173fa7ec610017
unknown <lsj8249@naver.com>
2021-12-03 20:46:24 +0900

    feat : 새로 올리온 게시글에 대한 토큰화+유사단어 처리

--- new_db.py
+++ new_db.py
@@ -4,20 +4,20 @@ import tqdm
 from pprint import pprint
 
 #### new_crawl_list :  
-new_crawl_list = [['일반공지', '[일반공지] 교내시설 대관 제한 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741219&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:47:58.928990'], 
-['일반공지', '[일반공지] 2021학년도 동계방학 메이커스페이스MARU 근로장학생 [추가...', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741193&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:47:59.712425'], 
-['일반공지', '[일반공지] 2022 에몬스 슬로건 공모전 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741183&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:00.500311'], 
-['일반공지', '[일반공지] 21 인공지능 학습용 데이터 온라인 해커톤 공모전 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741180&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:01.281635'], 
-['일반공지', '[일반공지] 2021학년도 2학기 학생증 신규발급 일시중단 안내(2022....', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741159&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:02.063466'], 
-['일반공지', '[일반공지] [DUICA]컴활, GTQ, 웹퍼블리셔 자격 특강', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741146&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:02.849209'], 
-['일반공지', '[일반공지] [중앙도서관-Making School] 메이커스페이스 MARU...', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741144&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:03.629960'], 
-['일반공지', '[일반공지]제1회 일반교양 학생 아이디어 공모전 수상작 발표', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741143&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:04.408793'], 
-['일반공지', '[일반공지] [카운슬링센터] 온라인 심리검사 프로그램 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741128&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0','2021-12-03 19:48:05.191510'], 
-['일반공지', '[일반공지] 2021년 동계 베트남 온라인 해외봉사 단원모집(재안내)', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741116&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:05.970713'], 
-['일반공지','[일반공지] 2021년 동계 필리핀 온라인 해외봉사 단원모집(재안내)', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741115&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:06.750168']]
+# new_crawl_list = [['일반공지', '[일반공지] 교내시설 대관 제한 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741219&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:47:58.928990'], 
+# ['일반공지', '[일반공지] 2021학년도 동계방학 메이커스페이스MARU 근로장학생 [추가...', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741193&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:47:59.712425'], 
+# ['일반공지', '[일반공지] 2022 에몬스 슬로건 공모전 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741183&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:00.500311'], 
+# ['일반공지', '[일반공지] 21 인공지능 학습용 데이터 온라인 해커톤 공모전 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741180&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:01.281635'], 
+# ['일반공지', '[일반공지] 2021학년도 2학기 학생증 신규발급 일시중단 안내(2022....', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741159&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:02.063466'], 
+# ['일반공지', '[일반공지] [DUICA]컴활, GTQ, 웹퍼블리셔 자격 특강', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741146&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:02.849209'], 
+# ['일반공지', '[일반공지] [중앙도서관-Making School] 메이커스페이스 MARU...', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741144&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:03.629960'], 
+# ['일반공지', '[일반공지]제1회 일반교양 학생 아이디어 공모전 수상작 발표', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741143&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:04.408793'], 
+# ['일반공지', '[일반공지] [카운슬링센터] 온라인 심리검사 프로그램 안내', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741128&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0','2021-12-03 19:48:05.191510'], 
+# ['일반공지', '[일반공지] 2021년 동계 베트남 온라인 해외봉사 단원모집(재안내)', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741116&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:05.970713'], 
+# ['일반공지','[일반공지] 2021년 동계 필리핀 온라인 해외봉사 단원모집(재안내)', 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26741115&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0', '2021-12-03 19:48:06.750168']]
 
 # 불용어 처리 & 토큰화
-def tokenized():
+def tokenized(new_crawl_list):
     stop_words= []
     with open('stopword.txt', encoding='utf-8') as f:
         for i in f:
@@ -34,9 +34,9 @@ def tokenized():
     return tokenized_data
 
 # 유사단어 찾기
-def findSimilar():
+def findSimilar(new_crawl_list):
     synonym = []
-    tokenized_data = tokenized()
+    tokenized_data = tokenized(new_crawl_list)
     model=Word2Vec.load('model/Kkma_dataset.model')
     for i in tokenized_data:
         for j in i :
@@ -46,6 +46,4 @@ def findSimilar():
                 pass
     return synonym
 
-
-
-pprint(findSimilar())
+# pprint(findSimilar(new_crawl_list))
