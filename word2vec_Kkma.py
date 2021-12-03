f4dc3543dd26bd89b214a2ce2b84bc2c071fb2ca
unknown <lsj8249@naver.com>
2021-12-03 19:02:45 +0900

    feat : 학습주기

--- word2vec_Kkma.py
+++ word2vec_Kkma.py
@@ -12,12 +12,16 @@ from gensim.models.word2vec import Word2Vec
 # from konlpy.tag import Okt
 from konlpy.tag import Kkma
 import tqdm
+import time
+import schedule
+
 
 
 engine = create_engine('postgresql://jrbysnbvqyvmie:4a2d878446a2864c6c7b9b16b965f58756035fea520bf6f682db34769ff6d053@ec2-44-198-236-169.compute-1.amazonaws.com:5432/db0sh1er7k2vqh')
 Session = sessionmaker()
 Session.configure(bind=engine)
 
+# 학습
 def findSynonym():
     session = Session()
     train_data=session.query(Crawl.title).all()
@@ -52,35 +56,52 @@ def findSynonym():
     vocabs=word_vectors.vocab.keys()# 사전
     word_vectors_list=[word_vectors[v] for v in vocabs]
     session.close()
-    #return model
+    return model
     
 def findLink(set, keyword): # 학습결과를 model로 저장함
     session = Session()
     model = Word2Vec.load(set)
     similar = model.wv.most_similar(keyword)#사업과 가장 유사한 단어 
     similar.append((keyword, 1))
-    print(similar)
     for i in similar:
-        train_data=session.query(Crawl.link).filter(Crawl.title.like('%'+i[0]+'%' or '%'+keyword+'%')).all()
-        # print(train_data)
+        links=session.query(Crawl.link).filter(Crawl.title.like('%'+i[0]+'%' or '%'+keyword+'%')).all()
     session.close()
+    return links
 
-def findsimilar(set,keyword):
-    model=Word2Vec.load(set)
-    most_similar=model.wv.most_similar(keyword)
-    print(set+"을 사용한 "+keyword+" 와(과) 관련된 유사도 : ")
-    print(most_similar) 
+# def findSimilar(set,keyword):
+#     model=Word2Vec.load(set)
+#     most_similar=model.wv.most_similar(keyword)
+#     print(set+"을 사용한 "+keyword+" 와(과) 관련된 유사도 : ")
+#     print(most_similar) 
 
-def findvocab(set):
+def findVocab(set):
     model=Word2Vec.load(set)
     
     word_vectors=model.wv
     vocabs=word_vectors.vocab.keys()# 사전
-
+    remove_key=[]
+    print("제거전")
+    print(vocabs)
+    for i in vocabs:
+        if len(i)==1:
+            remove_key.append(i)
+    for key in list(vocabs) : ## list와 keys()를 꼭 써야함.
+        if key in remove_key :
+            del word_vectors.vocab[key]
+    print("제거후")
+    vocabs=word_vectors.vocab.keys()
+    print(vocabs)   
     return vocabs   
   
+findVocab('model/Kkma_dataset.model')
 
-print(findvocab('model/Kkma_dataset.model'))
+schedule.every().monday.at("10:00").do(findSynonym) #매주 월요일 10시에 실행
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
 # findSynonym()
 # print("----------------기존 방법")
 # findLink('model/Okt_dataset.model', '편입학')
