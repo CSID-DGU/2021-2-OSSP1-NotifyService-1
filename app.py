0afc31aabb6a9aac4b2e18b6087c28c0ed17e5f5
Hoyasdf <alsghqlgodrl@gmail.com>
2021-12-15 18:19:42 +0900

    feat 1시간 단위로 크롤 호출

--- app.py
+++ app.py
@@ -519,5 +519,5 @@ def crawl():
     return "크롤링 페이지"
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=3000, debug=True)
+#if __name__ == '__main__':
+#    app.run('0.0.0.0', port=3000, debug=True)
