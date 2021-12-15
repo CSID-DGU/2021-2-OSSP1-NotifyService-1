777190e7e925fd0ce64e31512a3c90ba2cfcbdea
wonny7447 <wonny7447@naver.com>
2021-12-15 20:59:54 +0900

    add 발송 대상 없는경우 예외 처리

--- new_db.py
+++ new_db.py
@@ -187,31 +187,3 @@ def getUrl(path):
 
 def sendMany(data):
     return requests.post(getUrl('/messages/v4/send-many'), headers=get_headers(apiKey, apiSecret), json=data)
-
-
-# 한번 요청으로 1만건의 메시지 발송이 가능합니다.
-if __name__ == '__main__':
-    data = {
-        'messages': [
-            # 알림톡 발송
-            {
-                'to': ['01074477163'],  # array 사용으로 동일한 내용을 여러 수신번호에 전송 가능
-                'from': '01074477163',
-                'text': '카카오톡채널 테스트 지원❤',
-                'kakaoOptions': {
-                    'pfId': 'KA01PF211130075802780LDxZiwnOy9H'
-                    , 'buttons': [
-                        {
-                            'buttonType': 'WL',  # 웹링크
-                            'buttonName': '공지사항 보러가기',
-                            'linkMo': 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26740812&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0',
-                            'linkPc': 'https://www.dongguk.edu/mbs/kr/jsp/board/view.jsp?spage=1&boardId=3646&boardSeq=26740812&id=kr_010802000000&column=&search=&categoryDepth=&mcategoryId=0'
-                            # 템플릿 등록 시 모바일링크만 입력하였다면 linkPc 값은 입력하시면 안됩니다.
-                        }
-                    ]
-                }
-            }
-        ]
-    }
-    res = sendMany(data)
-    print(json.dumps(res.json(), indent=2, ensure_ascii=False))
